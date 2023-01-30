c4da03ab61a4517ba0fd8b1b8fc33fdeec37f4d2
Anusha <anusha.kandala@gmail.com>
2023-01-30 19:08:17 +0530

    Added few Lines to Java Program

--- Project/IntergerTest.java
+++ Project/IntergerTest.java
@@ -1,11 +1,14 @@
-Class main {
+public Class main {
 public static void main(String[] args) {
 
         int A = 10;
         int B = 20;
+        int C = 30
 
         // add two numbers
         int sum = A + B;
+        int sum1 = A + B + C;
         System.out.println(A + " + " + B + " = "  + sum);
+        System.out.println(A + " + " + B + " + " + C + " = "  + sum1);
         }
         }
