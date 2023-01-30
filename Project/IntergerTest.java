916efe8ac8bf33f0e56a9ed550018f5eb4f31516
Anusha <anusha.kandala@gmail.com>
2023-01-30 19:31:02 +0530

    Added new Lines in dummy branch

--- Project/IntergerTest.java
+++ Project/IntergerTest.java
@@ -3,9 +3,12 @@ public static void main(String[] args) {
 
         int A = 10;
         int B = 20;
+        int C = 5;
 
         // add two numbers
         int sum = A + B;
+        int sum1 = A + C;
         System.out.println(A + " + " + B + " = "  + sum);
+        System.out.println(A + " + " + C + " = "  + sum1);
         }
         }
