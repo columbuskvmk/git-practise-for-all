3966ef3ab57738dab92b9f5a2c213c56300d10e7
Murthy <murthycvsn@yahoo.com>
2023-03-28 10:07:47 +0530

    Commit Murthy.java to Master

---
+++ src/java/Murthy.java
@@ -0,0 +1,7 @@
+public class Murthy
+{
+    public static void main(String[] args) {
+        System.out.println("Hellow ...this is My first Commit to GIT");
+                        //Test
+    }
+}
